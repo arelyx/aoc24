e55be0ebb90ad14a5074df2170cc4d845aa59a1c
arelyx <39994376+arelyx@users.noreply.github.com>
2024-12-03 22:32:49 -0800

    attempt 1

---
+++ day4/p1/script.py
@@ -0,0 +1,80 @@
+import re
+
+crossword = []
+with open("input2.txt", "r") as f:
+    for line in f.readlines():
+        crossword.append(line.strip())
+
+sols = 0
+
+# horizontal forward
+for line in crossword:
+    sols += len(re.findall(r'(XMAS)', line))
+
+# horizontal backward
+for line in crossword:
+    sols += len(re.findall(r'(SAMX)', line))
+
+crossword_rot = []
+for x in range(len(crossword[0])):
+    crossword_rot.append('')
+
+for i, line in enumerate(crossword):
+    for j, char in enumerate(line):
+        crossword_rot[j] = crossword_rot[j]+char
+
+# vertical forward
+for line in crossword:
+    sols += len(re.findall(r'(XMAS)', line))
+
+# vertical backward
+for line in crossword_rot:
+    sols += len(re.findall(r'(SAMX)', line))
+
+crossword_rightshift = []
+for i, line in enumerate(crossword):
+    crossword_rightshift.append(i * " " + line + ((len(crossword[0]) - 1 - i) * " "))
+    # print(len(crossword_rightshift[-1]))
+
+crossword_rightshift_rot = []
+for x in range(len(crossword_rightshift[0])):
+    crossword_rightshift_rot.append('')
+
+for i, line in enumerate(crossword_rightshift):
+    for j, char in enumerate(line):
+        crossword_rightshift_rot[j] = crossword_rightshift_rot[j]+char
+
+# for line in crossword_rightshift_rot:
+#     print(line)
+
+# diagonal rightshift forward
+for line in crossword:
+    sols += len(re.findall(r'(XMAS)', line))
+
+# diagonal rightshift backward
+for line in crossword_rot:
+    sols += len(re.findall(r'(SAMX)', line))
+
+crossword_leftshift = []
+for i, line in enumerate(crossword):
+    crossword_leftshift.append(((len(crossword[0]) - 1 - i) * " ") + line + i * " ")
+    # print(len(crossword_leftshift[-1]))
+
+crossword_leftshift_rot = []
+for x in range(len(crossword_leftshift[0])):
+    crossword_leftshift_rot.append('')
+
+for i, line in enumerate(crossword_leftshift):
+    for j, char in enumerate(line):
+        crossword_leftshift_rot[j] = crossword_leftshift_rot[j]+char
+
+# diagonal leftshift forward
+for line in crossword:
+    sols += len(re.findall(r'(XMAS)', line))
+
+# diagonal leftshift backward
+for line in crossword_rot:
+    sols += len(re.findall(r'(SAMX)', line))
+
+
+print(sols)
