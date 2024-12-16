120f55e64b8c7e680350baa8cdbab5c11111fcbe
arelyx <39994376+arelyx@users.noreply.github.com>
2024-12-16 06:33:03 -0800

    d7 p2

--- day7/p2/script.py
+++ day7/p2/script.py
@@ -4,23 +4,11 @@ with open("input.txt", "r") as f:
         sum, nums = line.strip().split(":")
         sums.append([sum,nums.strip().split(" ")])
 
-# hitting the reset because it's been a week...
-
-# todo
-# helper function to get all permutations of a number set
-# helper function to get all possible sums (* or +) of a number set
-
-
-def get_all_perms(nums):
-    all_perms = []
-    for i in range(len(nums)):
-        if nums[0:i] != []:
-            all_perms.append([nums[0:i],nums[i:len(nums)]])
-    return all_perms
-
-def get_all_sums(nums):
+total = 0
+for sum in sums:
+    curr_total = int(sum[0])
     possible_sums = set()
-    for num in nums:
+    for num in sum[1]:
         if len(possible_sums) == 0:
             possible_sums.add(int(num))
         else:
@@ -28,26 +16,8 @@ def get_all_sums(nums):
             for curr_sum in list(possible_sums):
                 new_possible_sums.add(curr_sum * int(num))
                 new_possible_sums.add(curr_sum + int(num))
+                new_possible_sums.add(int(str(curr_sum) + str(num)))
             possible_sums = new_possible_sums
-    return possible_sums
-
-total = 0
-for sum in sums:
-    # print(sum)
-    curr_target = sum[0]
-    curr_numbers = sum[1]
-    if int(curr_target) in get_all_sums(curr_numbers):
-        total = total+int(curr_target)
-    else:
-        curr_perms = get_all_perms(curr_numbers)
-        for curr_perm in curr_perms:
-            left_all_sums = get_all_sums(curr_perm[0])
-            right_all_sums = get_all_sums(curr_perm[1])
-            for l_val in list(left_all_sums):
-                for r_val in list(right_all_sums):
-                    if str(l_val) + str(r_val) == str(curr_target):
-                        total = total+int(curr_target)
-
+    if curr_total in possible_sums:
+        total += curr_total
 print(total)
-
-# MISUNDERSTOOD!!
