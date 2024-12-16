e8e1236bd167c33f268c80fd273807d6a82bc130
arelyx <39994376+arelyx@users.noreply.github.com>
2024-12-16 09:28:04 -0800

    d9 p1

--- day9/p1/script.py
+++ day9/p1/script.py
@@ -4,35 +4,32 @@ with open("input.txt", "r") as f:
 
 is_file = True
 idx = 0
-disk = ""
+disk = []
 for size in disk_zip:
     if is_file:
-        disk = disk + str(idx) * int(size)
+        disk = disk + [str(idx)] * int(size)
         idx+=1
     else:
-        disk = disk + "." * int(size)
+        disk = disk + ["."] * int(size)
     is_file = not is_file
 
 disk = list(disk)
 disk_len = len(disk)
-# print(disk_len)
+next_free = 0
+next_move = disk_len -1
 while True:
-    next_free = None
-    next_move = None
-    for i, val in enumerate(disk):
-        if val == ".":
-            next_free = i
+    while True:
+        if disk[next_free] == ".":
             break
-    for i_offset, val in enumerate(reversed(disk)):
-        i = disk_len - i_offset - 1 
-        if val != ".":
-            next_move = i
-            break
-    disk[next_free], disk[next_move] = disk[next_move], disk[next_free]
-    if next_free == next_move - 1:
-        break
-    # print(len(str(next_move)))
+        next_free+=1
 
+    while True:
+        if disk[next_move] != ".":
+            break
+        next_move -= 1
+    if next_free >= next_move:
+        break
+    disk[next_free], disk[next_move] = disk[next_move], disk[next_free]
 cksum = 0
 for idx, f_id in enumerate(disk):
     if f_id.isnumeric():
